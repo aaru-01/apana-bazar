068839ca402608e6deaa99c00252e126cf3bbed0
aaru01 <aartimedhe16601@gmail.com>
2023-10-29 08:13:49 -0700

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+
+
+const app = express();
+app.use(express.json());
+
+const connectDB = async () =>{
+const conn = await mongoose.connect(process.env.MONGODB_URI);
+if (conn) {
+    console.log(`MONGODB Connected`);
+}
+};
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on Port ${PORT}`)
+    connectDB();
+});
